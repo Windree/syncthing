f8aa4d6e4e0d2a5c1f40a4a506ab7649a3ec7c04
Windree <windree@dragonsoul.com>
2024-10-21 13:58:14 +0000

    feat: use env to declare ports

--- ufw.sh
+++ ufw.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-for network in 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16 fe80::/64; do
-    ufw allow proto tcp to "$network" port 8384
+source "$(dirname "${BASH_SOURCE[0]}")/.env"
+set -Eeuo pipefail
+for remote in "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16" "fe80::/64"; do
+    ufw allow from "$remote" to any port "$SYNCTHING_WEB_PORT" proto tcp
 done
 
-ufw allow proto tcp to any port 22000
-ufw allow proto udp to any port 22000
-ufw allow proto udp to any port 21027
+ufw allow proto tcp to any port $SYNCTHING_DATA_PORT
+ufw allow proto udp to any port $SYNCTHING_BROADCAST_PORT
 
 ufw reload
